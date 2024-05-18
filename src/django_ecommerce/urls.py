f55a24bb09d4741e166f4d169199fda8ed0d0ffb
Enzo Nonato <enzononato10@gmail.com>
2024-05-18 02:54:19 -0300

    hello world

--- src/django_ecommerce/urls.py
+++ src/django_ecommerce/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from .views import home_page
 urlpatterns = [
-    path('admin/', admin.site.urls),
+        path('', home_page),
+        path('admin/', admin.site.urls),
 ]
