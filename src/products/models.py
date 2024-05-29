47e1a0f9a15918c36998c83c81178ab43ff5fa1a
Enzo Nonato <enzononato10@gmail.com>
2024-05-28 22:31:34 -0300

    adding a class Product in products/models.py

--- src/products/models.py
+++ src/products/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='products/images/')
+
+    def __str__(self):
+        return self.name
