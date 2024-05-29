82c3b734e817cfa33bcfd36173510c82b44af1c9
Enzo Nonato <enzononato10@gmail.com>
2024-05-28 22:57:41 -0300

    add models at admin.py and make the migrations

--- src/products/models.py
+++ src/products/models.py
@@ -4,7 +4,7 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='products/images/')
+    #image = models.ImageField(upload_to='products/images/')
 
     def __str__(self):
         return self.name
