d1776ff451747aadf6e57ea61e9a98ac72cfd75a
Enzo Nonato <enzononato10@gmail.com>
2024-06-05 04:50:01 -0300

    corrigindo o ultimo commit, adicionando a função de imagens dos produtos e a exclusão de todos os dados dos produtos quando estes forem excluidos. Também foi adicionado um grupo que pode adicionar e editar produtos sem serem administradores

--- src/products/admin.py
+++ src/products/admin.py
@@ -6,7 +6,8 @@ from django.shortcuts import redirect
 from django.contrib import messages
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'image_tag', 'delete_image_button')
+    list_display = ('name', 'price', 'image_tag', 'created_by')
+    readonly_fields = ('created_by',)  # torna o campo 'created_by' somente leitura, evitando alteração por parte do usuário
     
     def get_queryset(self, request):
         qs = super().get_queryset(request)
@@ -19,30 +20,30 @@ class ProductAdmin(admin.ModelAdmin):
             obj.created_by = request.user
         super().save_model(request, obj, form, change)
     
-    def image_tag(self, obj):
+    def image_tag(self, obj): #Renderiza a imagem em miniatura na lista de produtos.
         if obj.image:
             return format_html('<img src="{}" width="50" height="50" />'.format(obj.image.url))
         return "-"
     image_tag.short_description = 'Image'
     
-    def delete_image_button(self, obj):
+    def delete_image_button(self, obj): #Renderiza o botão de exclusão da imagem ao lado do produto.
         if obj.image:
             return format_html('<a class="button" href="{}">Delete Image</a>', self.get_delete_image_url(obj.id))
         return "-"
     delete_image_button.short_description = 'Delete Image'
     delete_image_button.allow_tags = True
 
-    def get_delete_image_url(self, obj_id):
+    def get_delete_image_url(self, obj_id): #Gera a URL para a ação de exclusão da imagem.
         return reverse('admin:delete_product_image', args=[obj_id])
     
-    def get_urls(self):
+    def get_urls(self): #Adiciona uma URL personalizada para a ação de exclusão da imagem.
         urls = super().get_urls()
         custom_urls = [
             path('delete-image/<int:product_id>/', self.admin_site.admin_view(self.delete_image), name='delete_product_image'),
         ]
         return custom_urls + urls
 
-    def delete_image(self, request, product_id):
+    def delete_image(self, request, product_id): #Lida com a lógica de exclusão da imagem do produto e exibe uma mensagem de sucesso.
         product = self.get_object(request, product_id)
         if product.image:
             product.image.delete()
